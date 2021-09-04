2cb2ccb5ea33368f7756613cf19603df8bf36583
Gabriel Dechichi <gabrieldechichi@gmail.com>
2021-09-04 13:02:01 -0600

    45 - Classes

---
+++ 45_Classes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _45_Classes
+{
+    class Player
+    {
+        public string name = "NONAME";
+        public int level = 1;
+
+        public void Test()
+        {
+            Console.WriteLine($"{name}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            Player p1 = new Player();
+            Console.WriteLine($"Nome do Player {p1.name}, Level do Player {p1.level}");
+            p1.level = 10;
+            Console.WriteLine($"Level do player: {p1.level}");
+
+            Console.ReadKey();
+        }
+    }
+}
