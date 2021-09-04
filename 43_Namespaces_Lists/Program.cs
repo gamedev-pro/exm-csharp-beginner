facb75e0e63138fc30a35f7f8df506eda4174a4c
Gabriel Dechichi <gabrieldechichi@gmail.com>
2021-09-04 13:01:24 -0600

    43 - Namespaces and Lists

---
+++ 43_Namespaces_Lists/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+//named spaces: espacos nomeados
+//eles definem um escopo para um conjunto de classes e funcoes
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.ReadKey();
+        //arrays 
+        int[] myArray = new int[3];
+        myArray[0] = 1;
+        myArray[1] = 2;
+        //funcionalidade para mudar de tamanho
+        List<int> myList = new List<int>(3);
+        myList.Add(-23);
+        myList.Add(-23);
+        myList.Add(-23);
+        myList.Add(-23);
+        myList.Add(-23);
+        myList.Add(-23);
+        myList.Add(-23);
+        myList.RemoveAt(4);
+        //comando -> funcoes -> classes -> namespaces
+    }
+}
