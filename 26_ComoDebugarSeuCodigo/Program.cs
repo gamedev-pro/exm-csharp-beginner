ab42641d914d66461708b3068f596c659a3f8fc0
Gabriel Dechichi <gabrieldechichi@gmail.com>
2021-09-04 12:56:25 -0600

    26 - Como Debugar seu Codigo

---
+++ 26_ComoDebugarSeuCodigo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1._2_ErrosDeLogica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira seu nome: ");
+            string nome = Console.ReadLine();
+            nome = nome.Trim();
+            Console.Write("Seu nome eh -> ");
+            Console.WriteLine(nome);
+
+            Console.ReadKey();
+        }
+    }
+}
