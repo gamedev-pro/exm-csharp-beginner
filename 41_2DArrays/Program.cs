7c806d06a93e0967111f955bf1624c942e0f2466
Gabriel Dechichi <gabrieldechichi@gmail.com>
2021-09-04 13:00:50 -0600

    41 - 2D Arrays

---
+++ 41_2DArrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _41_2DArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
