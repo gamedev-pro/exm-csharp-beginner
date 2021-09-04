5f121a0895fee6f54b1bab5bd99554bc55ada1af
Gabriel Dechichi <gabrieldechichi@gmail.com>
2021-09-04 12:54:58 -0600

    25 - Erro de Sintaxe

---
+++ 25_ErrosDeSintaxe/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1._1_ErrosDeSintaxe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Passo a Passo - Erros de Sintaxe
+            //1 - Entender a MENSAGEM
+            //2 - Ir ate a linha da mensagem e ver se voce ja consegue resolver o erro
+            //3 - Pesquisar o erro na internet em INGLES
+            // Lista do Expert Mode
+            //4 - Voce pergunta na internet, para um amigo, na Comunidade do Expert Mode
+            int x = 2;
+        }
+    }
+}
