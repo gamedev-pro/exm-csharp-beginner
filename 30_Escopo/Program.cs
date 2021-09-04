49ae8d952ce6254eb3b906dbff00409fb2f9f7ab
Gabriel Dechichi <gabrieldechichi@gmail.com>
2021-09-04 12:57:26 -0600

    30 - Escopo

---
+++ 30_Escopo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _3_Escopo
+{
+    class Program
+    {
+        static int variavelDaClasse;
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Teste();
+            string mensagem = "Oi!";
+            Console.WriteLine(variavelDaClasse);
+
+
+            {
+                //fala pro sistema operacional aloca um pedaco de memoria
+                //para a variavel x
+                int x = 3;//definicao de variavel
+
+            }
+
+            x = 2;
+            //apagada
+        }
+
+        static void Teste()
+        {
+            int variavelDaFuncao = 2;
+            Console.WriteLine(variavelDaClasse);
+        }
+    }
+}
